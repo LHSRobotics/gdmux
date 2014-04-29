2974f9493f639ef213dc88bd89fb2e1a05a3ed8e
Salmān Aljammāz <s@0x65.net>
2014-04-29 19:10:19 +0100

    vplu-send

---
+++ cmd/vplus-send/main.go
@@ -0,0 +1,59 @@
+// Command vplus automates dealing with the V+ console.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var terminal = flag.String("terminal", "/dev/staubli-terminal", "the device file for the Staubli's termnial")
+
+func main() {
+	flag.Parse()
+
+	/*term, err := os.OpenFile(*terminal, os.O_APPEND|os.O_RDWR, os.ModeDevice)
+	if err != nil {
+		log.Println("error opening device file:", err)
+		os.Exit(1)
+	}*/
+	term := os.Stdout
+
+file:
+	for _, fname := range flag.Args() {
+		content, err := ioutil.ReadFile(fname)
+		if err != nil {
+			log.Println("error opening file:", err)
+			continue
+		}
+
+		_, err = term.WriteString(fmt.Sprintf("edit %s\r\n", fname))
+		if err != nil {
+			log.Printf("error sending file (%v): %v", fname, err)
+			continue file
+		}
+
+		buf := make([]byte, 1)
+		for _, b := range content {
+			if b == '\n' {
+				_, err = term.WriteString("\r\n")
+				if err != nil {
+					log.Printf("error sending file (%v): %v", fname, err)
+					continue file
+				}
+			} else {
+				buf[0] = b
+				_, err = term.Write(buf)
+			}
+		}
+
+		_, err = term.WriteString("e\r\n")
+		if err != nil {
+			log.Printf("error sending file (%v): %v", fname, err)
+			continue file
+		}
+	}
+	term.Close()
+}
